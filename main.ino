b53828ad7433eee58b0e945380e97090f2e0c9dd
987sebasBeller <sebastianbeller76@gmail.com>
2024-03-09 22:41:18 -0400

    The constants file is added

--- main.ino
+++ main.ino
@@ -1,13 +1,7 @@
 #include "Esp32.h"
 #include "UltrasonicSensor.h"
 #include<Arduino.h>
-const int TRIGGER_PIN=12;
-const int ECHO_PIN=13;
-const int LEDS_COUNT=6;
-const int TRANSMISSION_SPEED=9600;
-const int INTERVAL=7;
-const int SAMPLE_SIZE=1;
-const int ledPins[LEDS_COUNT] = {15,2,4,5,18,19};
+
 Esp32 esp;
 UltrasonicSensor ultrasonicSensor(TRIGGER_PIN,ECHO_PIN);
 void setup()
