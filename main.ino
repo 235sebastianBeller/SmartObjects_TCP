9f7146b44f1dfc82f4d0420e75c6b7c7de9652e3
987sebasBeller <sebastianbeller76@gmail.com>
2024-03-09 22:39:49 -0400

    The Esp32 class is added

--- main.ino
+++ main.ino
@@ -1,18 +1,26 @@
+#include "Esp32.h"
 #include "UltrasonicSensor.h"
-#include "Led.h"
 #include<Arduino.h>
-const int TRIGGER_PIN=4;
-const int ECHO_PIN=2;
+const int TRIGGER_PIN=12;
+const int ECHO_PIN=13;
+const int LEDS_COUNT=6;
+const int TRANSMISSION_SPEED=9600;
+const int INTERVAL=7;
+const int SAMPLE_SIZE=1;
+const int ledPins[LEDS_COUNT] = {15,2,4,5,18,19};
+Esp32 esp;
+UltrasonicSensor ultrasonicSensor(TRIGGER_PIN,ECHO_PIN);
 void setup()
-{
-  Led myLed(7);
-  Serial.begin(9600);
+{ 
+  esp.initializeLeds();
+  Serial.begin(TRANSMISSION_SPEED);
 }
 
 void loop()
 {
-  UltrasonicSensor ultrasonicSensor(TRIGGER_PIN,ECHO_PIN);
-  int cm=ultrasonicSensor.getTheShortestDistance(10);
-    Serial.print(cm);
-    Serial.println("cm");
+  int cm=ultrasonicSensor.getTheShortestDistance(SAMPLE_SIZE);
+  //Serial.println(cm);
+  int countLedsOn=min(cm/INTERVAL,6);
+  Serial.println(countLedsOn);
+  esp.turnOnTheLeds(countLedsOn);
 }
