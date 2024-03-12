03ac2ee03f8290c933e2618b0a6beca0ae52a387
987sebasBeller <121454971+987sebasBeller@users.noreply.github.com>
2024-03-12 10:23:56 -0400

    Update constants.h

--- constants.h
+++ constants.h
@@ -5,6 +5,6 @@ const int ECHO_PIN=13;
 const int LEDS_COUNT=6;
 const int TRANSMISSION_SPEED=9600;
 const int INTERVAL=7;
-const int SAMPLE_SIZE=1;
+const int SAMPLE_SIZE=10;
 const int ledPins[LEDS_COUNT] = {15,2,4,5,18,19};
 #endif 
