ba7873d5b87908e916c272c800730143d04241c3
987sebasBeller <sebastianbeller76@gmail.com>
2024-03-29 13:22:19 -0400

    The main.ino file is modified

---
+++ SmartObjects/SmartObjectActuators/main.ino
@@ -0,0 +1,29 @@
+#include "Esp32.h"
+#include <WiFi.h>
+#include "ClientHandlerLeds.h"
+#include "ClientHandlerLcd.h"
+#include "constants.h"
+
+Esp32 esp;
+void setup() {
+  Serial.begin(TRANSMISSION_SPEED);
+  esp.initializeLeds();
+  esp.intilizeLcd();
+  esp.connectToInternet(WIFI_SSID, WIFI_PASS);
+  Serial.print("\n My IP address: ");
+  Serial.println(esp.getIpAdress());
+}
+
+void loop() {
+  delay(1000);
+  ClientHandler *clientList[CLIENTS_ACTUATORS_COUNT] = { new ClientHandlerLeds(REQUEST_CLIENT_HANDLER_LEDS), new ClientHandlerLcd(REQUEST_CLIENT_HANDLER_LCD) };
+  for (int i = 0; i < CLIENTS_ACTUATORS_COUNT; i++) {
+    ClientHandler *clientHdl = clientList[i];
+    clientHdl->connectoToTheServer(SERVER_ADDRESS, SERVER_PORT);
+    clientHdl->sendRequestToTheServer();
+    clientHdl->getResponseFromServer(esp);
+    clientHdl->closeConnectionFromTheServer();
+    delete clientHdl;
+  }
+  delete[] clientList;
+}
